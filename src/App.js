b2ca73b6f9bc1698c7fe0169651b8ee687e9bb4a
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-02-01 20:20:36 +0530

    changes in useState hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { useState } from "react";
 import Stepper from "./components/Stepper";
 import StepperController from "./components/StepperController";
+import Account from "./components/steps/Account";
+import Details from "./components/steps/Details";;
+import Final from "./components/steps/Final";;
+import Payment from "./components/steps/Payment";
+
 function App() {
+
+  const [currentStep, setCurrentStep] = useState(1)
 
   const steps=[
     "Account Information",
@@ -13,12 +21,19 @@ function App() {
     switch(step){
       case 1:
         return <Account/>
+        case 2:
+        return <Details/>
+        case 3:
+        return <Final/>
+        default: 
   }
   return (
     <div className="md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
       {}
       <div className="container horizontal mt-5">
-        <Stepper />
+        <Stepper 
+        steps={steps}
+        currentStep={currentStep}/>
       </div>
       {}
       <StepperController />
