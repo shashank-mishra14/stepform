c871d1ce471b1fe91b4e57ab12407823d7369689
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-02-01 19:51:51 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Stepper from "./components/Stepper";
+import StepperController from "./components/StepperController";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
+      {}
+      <div className="container horizontal mt-5">
+        <Stepper />
+      </div>
+      {}
+      <StepperController />
     </div>
   );
 }
