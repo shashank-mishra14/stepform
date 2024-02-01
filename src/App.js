f650f74487f108ba510be92ac71c21a5c53efaf1
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-02-01 20:12:03 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React from "react";
 import Stepper from "./components/Stepper";
 import StepperController from "./components/StepperController";
 function App() {
+
+  const steps=[
+    "Account Information",
+    "Personal Information",
+    "Complete"
+  ]
+
+  const displaySteps = (step)=>{
+    switch(step){
+      case 1:
+        return <Account/>
+  }
   return (
     <div className="md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
       {}
--- src/components/Stepper.jsx
+++ src/components/Stepper.jsx
@@ -1,24 +1,30 @@
 import React from "react";
-
+const displaySteps = (
+  <div  className="w-full flex items-center">
+    <div className="relative flex flex-col items-center text-teal-600">
+      <div
+        className="rounded-full transition duration-500 ease-in-out border-2 
+        border-gray-300 h-12 w-12 flex items-center
+        justify-center py-3"
+      >
+        {/*display number*/}
+      </div>
+      <div
+        className="absolute top-0 text-center mt-16 
+        w-32 text-xs font-medium uppercase"
+      >
+        {/*display desc*/}
+      </div>
+    </div>
+    <div className="flex-auto border-t-2 transition duration-500 ease-in-out">
+      {/*displayline*/}
+    </div>
+  </div>
+);
 const Stepper = () => {
   return (
     <div className="mx-4 p-4 flex justify-between items-center">
-      <div className="relative flex flex-col items-center text-teal-600">
-        <div
-          className="rounded-full transition duration-500 ease-in-out border-2 
-        border-gray-300 h-12 w-12 flex items-center
-        justify-center py-3"
-        >
-          {/*display number*/}
-        </div>
-        <div
-          className="absolute top-0 text-center mt-16 
-        w-32 text-xs font-medium uppercase"
-        >
-          {/*display desc*/}
-        </div>
-      </div>
-      <div className="flex-auto border-t-2 transition duration-500 ease-in-out">{/*displayline*/}</div>
+      {displaySteps}
     </div>
   );
 };
