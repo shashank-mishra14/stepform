4db06e505418a35d5a60f6c66d5fabb3a6abfc90
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-02-02 22:51:33 +0530

    changes

--- src/components/StepperController.jsx
+++ src/components/StepperController.jsx
@@ -5,7 +5,7 @@ const StepperController = ({ handleClick, currentStep, steps }) => {
     <div className="container flex justify-around mt-4 mb-8">
       <button
         onClick={() => {
-          handleClick("prev");
+          handleClick("");
         }}
         className="bg-white text-slate-400 uppercase py-2 px-4 rounder-xl font-semibold cursor-pointer border-slate-300 hover:bg-slate-700 hover:text-white transition duration-200 ease-in-out"
       >
@@ -19,7 +19,7 @@ const StepperController = ({ handleClick, currentStep, steps }) => {
           currentStep === 1 ? " opacity-50 cursor-not-allowed" : ""
         }`}
       >
-        {currentStep === steps.length - 1 ? "Submit" : "Next"}
+        {currentStep === steps.length - 1 ? "Confirm" : "Next"}
       </button>
     </div>
   );
--- src/components/steps/Account.jsx
+++ src/components/steps/Account.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useContext } from 'react'
+import { StepperContext } from '../../contexts/StepperContext'
+
 
 export default function Account() {
   return (
